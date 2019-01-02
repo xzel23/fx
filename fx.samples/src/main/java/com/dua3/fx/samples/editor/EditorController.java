37c23287bb2a9c7963b4c685f0d17e30c02efb5a
Axel Howind <axel@dua3.com>
2019-01-02 14:49:20 +0100

    support different encodings

--- fx.samples/src/main/java/com/dua3/fx/samples/editor/EditorController.java
+++ fx.samples/src/main/java/com/dua3/fx/samples/editor/EditorController.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.logging.Level;
 
 import com.dua3.fx.application.FxController;
@@ -29,21 +30,6 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
 
 public class EditorController extends FxController<EditorApp, EditorController> {
-
-	/** The default character encoding. */
-	private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
-
-	/** The character encodings used to load files. */
-	private static final List<Charset> CHARSETS;
-
-	static {
-		// setup list of charset; use a set to avoif having duplicate entries
-		Set<Charset> charsets = new LinkedHashSet<>();
-		charsets.add(StandardCharsets.UTF_8);
-		charsets.add(Charset.defaultCharset());
-		charsets.add(StandardCharsets.ISO_8859_1);
-		CHARSETS = new ArrayList<>(charsets);
-	}
 
 	private Charset charset = StandardCharsets.UTF_8;
 
@@ -59,37 +45,7 @@ public class EditorController extends FxController<EditorApp, EditorController> 
 	protected void openDocument(URI uri) throws IOException {
 		Path path = Paths.get(uri);
 		
-		String content = null;
-		ByteBuffer data = ByteBuffer.wrap(Files.readAllBytes(path));
-		data.mark();
-		for (Charset cs : CHARSETS) {
-			try {
-				data.reset();
-				content = cs.newDecoder()
-					.decode(data)
-					.toString();
-				charset = cs;
-				LOG.info(() -> String.format("loaded '%s' using charset %s", uri, charset));
-				break;
-			} catch (CharacterCodingException e) {
-				LOG.fine(() -> String.format("could not decode '%s' using charset %s", uri, cs));
-			}
-		}
-
-		if (content==null) {
-			if (!Dialogs
-				.confirmation("The character encoding of '%s' could not be determined. Open anyway?")
-				.showAndWait()
-				.equals(ButtonType.YES)) {
-				LOG.info(() -> String.format("encoding problem - not loading '%s'", uri));
-				return;
-			}
-
-			charset = DEFAULT_CHARSET;
-			LOG.info(() -> "encoding problem - loading as "+charset.name());
-			content = new String(data.array(), charset);
-		}
-
+		String content = IOUtil.loadText(path, cs -> charset=cs);
 		String extension = IOUtil.getExtension(path);
 		editor.setText(content, extension);
 		editor.setReadOnly(!Files.isWritable(path));
