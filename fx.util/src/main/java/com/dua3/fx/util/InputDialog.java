cb5ec0871c7b983623dd2922de03470865c0f8b0
Axel Howind <axel@dua3.com>
2019-02-03 20:17:47 +0100

    InputDialog, samples

---
+++ fx.util/src/main/java/com/dua3/fx/util/InputDialog.java
@@ -0,0 +1,101 @@
+// Copyright 2019 Axel Howind
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.dua3.fx.util;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Control;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class InputDialog extends Dialog<Map<String, Object>> {
+
+	interface InputControl<T> {
+		Control control();
+
+		T get();
+
+		void set(T arg);
+	}
+
+	static class Meta<T> {
+		final String id;
+		final String label;
+		final Class<T> cls;
+		final T dflt;
+		final Supplier<InputControl<T>> createControl;
+		T value;
+
+		Meta(String id, String label, Class<T> cls, T dflt, Supplier<InputControl<T>> createControl) {
+			this.id = id;
+			this.label = label;
+			this.cls = cls;
+			this.dflt = dflt;
+			this.value = dflt;
+			this.createControl = createControl;
+		}
+	}
+
+	private Collection<Meta<?>> data = null;
+
+    public InputDialog() {
+		setResultConverter(btn -> {
+			if (btn!=ButtonType.OK) {
+				return null;
+			}
+			return data.stream().collect(Collectors.toMap(e -> e.id, e -> e.value));
+		});
+     }
+
+     private void addToGrid(GridPane grid, Control child, int c, int r, Insets insets) {
+		grid.add(child, c, r);
+		GridPane.setMargin(child, insets);
+	}
+
+	public void setContent(Collection<Meta<?>> data, int columns) {
+		this.data = Objects.requireNonNull(data);
+		
+		// create grid with input controls
+		GridPane grid = new GridPane();
+		Insets insets = new Insets(2);
+		int r = 0, c = 0;
+		for (var entry : data) {
+			// add label and control
+			addToGrid(grid, new Label(entry.label), 2 * c, r, insets);
+			addToGrid(grid, entry.createControl.get().control(), 2 * c + 1, r, insets);
+
+			// move to next position in grid
+			c = (c + 1) % columns;
+			if (c == 0) {
+				r++;
+			}
+		}
+
+		DialogPane dialogPane = getDialogPane();
+		
+		dialogPane.setContent(grid);
+
+		dialogPane.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+	}
+
+}
---
+++ fx.util/src/main/java/com/dua3/fx/util/PlatformHelper.java
@@ -0,0 +1,46 @@
+package com.dua3.fx.util;
+
+import java.util.concurrent.CountDownLatch;
+
+import javafx.application.Platform;
+
+public class PlatformHelper {
+
+	/**
+	 * Runs the specified {@link Runnable} on the
+	 * JavaFX application thread and waits for completion.
+	 *
+	 * @param action the {@link Runnable} to run
+	 * @throws NullPointerException if {@code action} is {@code null}
+	 */
+	public static void runAndWait(Runnable action) {
+	    if (action == null)
+	        throw new NullPointerException("action");
+
+	    // run synchronously on JavaFX thread
+	    if (Platform.isFxApplicationThread()) {
+	        action.run();
+	        return;
+	    }
+
+	    // queue on JavaFX thread and wait for completion
+	    final CountDownLatch doneLatch = new CountDownLatch(1);
+	    Platform.runLater(() -> {
+	        try {
+	            action.run();
+	        } finally {
+	            doneLatch.countDown();
+	        }
+	    });
+
+	    try {
+	        doneLatch.await();
+	    } catch (InterruptedException e) {
+	        // ignore exception
+	    }
+	}
+	
+	private PlatformHelper() {
+		// utility class
+	}
+}
