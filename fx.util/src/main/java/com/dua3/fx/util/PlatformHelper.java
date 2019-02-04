e721e5cecbe7219cbae9175257ed0e772dfca2c3
Axel Howind <axel@dua3.com>
2019-02-04 06:36:42 +0100

    PlatformHelper.runAndWait()

--- fx.util/src/main/java/com/dua3/fx/util/PlatformHelper.java
+++ fx.util/src/main/java/com/dua3/fx/util/PlatformHelper.java
@@ -1,33 +1,34 @@
 package com.dua3.fx.util;
 
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 import javafx.application.Platform;
 
 public class PlatformHelper {
 
 	/**
-	 * Runs the specified {@link Runnable} on the
-	 * JavaFX application thread and waits for completion.
+	 * Run task on the JavaFX application thread and return result.
 	 *
-	 * @param action the {@link Runnable} to run
+	 * @param action the task to run
 	 * @throws NullPointerException if {@code action} is {@code null}
 	 */
-	public static void runAndWait(Runnable action) {
-	    if (action == null)
-	        throw new NullPointerException("action");
+	public static <T> T runAndWait(Supplier<T> action) {
+		Objects.requireNonNull(action);
 
 	    // run synchronously on JavaFX thread
 	    if (Platform.isFxApplicationThread()) {
-	        action.run();
-	        return;
+	        return action.get();
 	    }
 
 	    // queue on JavaFX thread and wait for completion
+	    AtomicReference<T> result = new AtomicReference<>();
 	    final CountDownLatch doneLatch = new CountDownLatch(1);
 	    Platform.runLater(() -> {
 	        try {
-	            action.run();
+	            result.set(action.get());
 	        } finally {
 	            doneLatch.countDown();
 	        }
@@ -38,6 +39,18 @@ public class PlatformHelper {
 	    } catch (InterruptedException e) {
 	        // ignore exception
 	    }
+	    
+	    return result.get();
+	}
+	
+	/**
+	 * Run task on the JavaFX application thread and wait for completion.
+	 *
+	 * @param action the task to run
+	 * @throws NullPointerException if {@code action} is {@code null}
+	 */
+	public static void runAndWait(Runnable action) {
+		runAndWait(() -> { action.run(); return null; });
 	}
 	
 	private PlatformHelper() {
