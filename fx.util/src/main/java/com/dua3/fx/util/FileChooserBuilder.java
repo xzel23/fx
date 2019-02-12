284ed8f1cb53627f9a43f68f4a260323399c0bba
Axel Howind <axel@dua3.com>
2019-02-12 21:21:01 +0100

    add javadoc

--- fx.util/src/main/java/com/dua3/fx/util/FileChooserBuilder.java
+++ fx.util/src/main/java/com/dua3/fx/util/FileChooserBuilder.java
@@ -38,12 +38,26 @@ public class FileChooserBuilder {
 
 	FileChooserBuilder() {
 	}
-	
+
+	/**
+	 * Show "Open" dialog.
+	 * @param parent
+	 *  the parent window
+	 * @return
+	 *  an Optional containing the selected file.
+	 */
 	public Optional<File> showOpenDialog(Window parent) {
 		FileChooser chooser = build();
 		return Optional.ofNullable(chooser.showOpenDialog(parent));
 	}
 
+	/**
+	 * Show "Save" dialog.
+	 * @param parent
+	 *  the parent window
+	 * @return
+	 *  an Optional containing the selected file.
+	 */
 	public Optional<File> showSaveDialog(Window parent) {
 		FileChooser chooser = build();
 		return Optional.ofNullable(chooser.showSaveDialog(parent));
@@ -60,32 +74,86 @@ public class FileChooserBuilder {
 		return chooser;
 	}
 
+	/**
+	 * Set initial directory.
+	 * @param initialDir
+	 *  the initial directory
+	 * @return
+	 *  this instance
+	 */
 	public FileChooserBuilder initialDir(File initialDir) {
 		this.initialDir = Objects.requireNonNull(initialDir);
 		return this;
 	}
 
+	/**
+	 * Set initial filename.
+	 * @param initialFileName
+	 *  the initial filename
+	 * @return
+	 *  this instance
+	 */
 	public FileChooserBuilder initialFileName(String initialFileName) {
 		this.initialFileName = Objects.requireNonNull(initialFileName);
 		return this;
 	}
-	
+
+	/** 
+	 * Add filter to the list of filters.
+	 * 
+	 * @param name
+	 *  the filter name
+	 * @param pattern
+	 *  the pattern(s) to use for this filter
+	 * @return
+	 *  this instance
+	 */
 	public FileChooserBuilder addFilter(String name, String... pattern) {
 		ExtensionFilter f = new ExtensionFilter(name, pattern);
 		filters.add(f);
 		return this;
 	}
 
+	/**
+	 * Set filters.
+	 * <p>
+	 * The current filters will be replaced.
+	 * 
+	 * @param filters
+	 *  the filters to set
+	 * @return
+	 *  this instance
+	 */
 	public FileChooserBuilder filter(List<ExtensionFilter> filters) {
 		this.filters = new LinkedList<>(filters);
 		return this;
 	}
 
+	/**
+	 * Set filters.
+	 * <p>
+	 * The current filters will be replaced.
+	 * 
+	 * @param filters
+	 *  the filters to set
+	 * @return
+	 *  this instance
+	 */
 	public FileChooserBuilder filter(ExtensionFilter... filters) {
 		this.filters = new LinkedList<>(Arrays.asList(filters));
 		return this;
 	}
 
+	/**
+	 * Set selected filter.
+	 * <p>
+	 * The filter is appended to the list of filters if not present.
+	 * 
+	 * @param f
+	 *  the selected filter
+	 * @return
+	 *  this instance
+	 */
 	public FileChooserBuilder selectedFilter(ExtensionFilter f) {
 		this.selectedFilter = f;
 		if (f != null && !filters.contains(f)) {
