97e3eeb250b32bca30bb669857ac3dd8e75005d0
Axel Howind <axel@dua3.com>
2019-06-26 08:46:37 +0200

    add bash scripts

---
+++ fx-editors/node/create_bundle.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+DIR=`dirname $0`
+DIST_DIR=${DIR}/dist
+TARGET_DIR=${DIR}/../src/main/resources/com/dua3/fx/editors/intern
+
+
+cd ${DIR}
+
+echo "running webpack ..."
+npx webpack --mode=production || exit 1
+
+echo "copying files to ${TARGET_DIR} ..."
+cp ${DIST_DIR}/code_editor.bundle.js ${TARGET_DIR}/ || exit 1
+cp ${DIST_DIR}/code_editor.html ${TARGET_DIR}/ || exit 1
+
+echo "SUCCESS"
--- fx-editors/node/src/code_editor.js
+++ fx-editors/node/src/code_editor.js
@@ -25,3 +25,171 @@ export const code_editor = CodeMirror.fromTextArea(document.getElementById('code
     lineNumbers : true,
     inputStyle : 'textarea'
 });
+
+// connect to logger
+console.log = function(m) {
+    bridge.log(m);
+};
+
+// set to true to enable trace messages
+var debug = true;
+
+// output trace messages in debug mode
+export function trace(m) {
+    if (debug) {
+        console.log("TRACE " + m);
+    }
+}
+
+// Set the editor content. Called from Java.
+export function jSetContent(text, ext) {
+    var mode = getModeFromExtension(ext);
+    CodeMirror.autoLoadMode(code_editor, mode.mode);
+
+    // force reset of placeholder (because it sometime fails to update when setting content)
+    var placeholder = code_editor.getOption("placeholder");
+    code_editor.setOption("placeholder", "");
+
+    code_editor.swapDoc(CodeMirror.Doc(text, mode.mime));
+
+    code_editor.setOption("placeholder", placeholder);
+
+    // inform Java code that the buffer is clean
+    bridge.setDirty(false);
+    console.log("LOAD: updated editor content, ext=" + ext + ", mode="
+        + mode.mode);
+}
+
+// Paste text at current position. Called from Java code.
+export function jReplaceSelection(text) {
+    trace("PASTING");
+    code_editor.replaceSelection(text);
+    trace("PASTED");
+}
+
+const mode_info_text = CodeMirror.findModeByExtension('txt');
+export function getModeFromExtension(ext) {
+    var info = CodeMirror.findModeByExtension(ext);
+    if (info) {
+        return info;
+    } else {
+        return mode_info_text;
+    }
+}
+
+export function jSetModeFromExtension(ext) {
+    var mode = getModeFromExtension(ext);
+    code_editor.setOption("mode", mode.mime);
+    CodeMirror.autoLoadMode(code_editor, mode.mode);
+    trace("jSetModeFromExtension: mode set to " + mode.mode);
+}
+
+// set readonly mode
+export function jSetReadOnly(flag) {
+    code_editor.setOption("readOnly", flag);
+    trace("jSetReadOnly: readOnly = " + flag);
+}
+
+// set the placeholder text
+export function jSetPromptText(text) {
+    code_editor.setOption("placeholder", text);
+    trace("jSetPromptText: promptText = '"+text+"'");
+}
+
+// use the system clipboard for cut & paste
+export function jPaste() {
+    trace("jPaste()");
+    bridge.paste();
+}
+
+export function jCopy() {
+    var text = code_editor.getSelection();
+    trace("jCopy(): '"+text+"'");
+    var arg = {
+        'format' : 'text',
+        'content' : text
+    };
+    bridge.copy(arg);
+}
+
+export function jCut() {
+    var text = code_editor.getSelection();
+    trace("jCut(): '"+text+"'");
+    var arg = {
+        'format' : 'text',
+        'content' : text
+    };
+    bridge.cut(arg);
+}
+
+export function jGetText() {
+    return code_editor.getDoc().getValue();
+}
+
+export function jGetLineCount() {
+    return code_editor.lineCount();
+}
+
+export function jGetLine(idx) {
+    return code_editor.getLine(idx);
+}
+
+export function jGetLineNumber(idx) {
+    return code_editor.getCursor(idx).line;
+}
+
+export function jSearch() {
+    code_editor.execCommand("find");
+}
+
+export function jSetShowLineNumbers(flag) {
+    trace('lineNumbers: '+flag);
+    code_editor.setOption('lineNumbers', flag);
+}
+
+export function jIsShowLineNumbers() {
+    return code_editor.getOption('lineNumbers');
+}
+
+export function jSetHighlightCurrentLine(flag) {
+    trace('styleActiveLine: '+flag);
+    code_editor.setOption('styleActiveLine', flag);
+}
+
+export function jIsHighlightCurrentLine() {
+    return code_editor.getOption('styleActiveLine');
+}
+
+export function jSetFontSize(size) {
+    code_editor.getWrapperElement().style["font-size"] = size+"px";
+}
+
+export function jGetFontSize() {
+    szs = code_editor.getWrapperElement().style["font-size"];
+    return parseFloat(szs.replace("px",""));
+}
+
+export function jSetTheme(theme) {
+    code_editor.setOption('theme', theme);
+}
+
+export function jGetTheme() {
+    return code_editor.gcetOption('theme');
+}
+
+export function jSetLine(i,s) {
+    code_editor.replaceRange(s, {line: i, ch: 0}, {line: i});
+}
+
+export function jAddLine(s) {
+    code_editor.replaceRange(s+'\n', {line: Infinity, ch: 0});
+}
+
+// track dirty state
+code_editor.on('change', function() {
+    bridge.setDirty(true);
+});
+
+jSetFontSize(14);
+
+const editor_initialised = true;
