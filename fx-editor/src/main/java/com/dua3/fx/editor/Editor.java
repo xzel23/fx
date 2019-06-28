271d7a2ffaffccda4ba425ee24b50fb98be18b35
Axel Howind <axel@dua3.com>
2019-06-28 08:48:23 +0200

    add editor as subproject

---
+++ fx-editor/src/main/java/com/dua3/fx/editor/Editor.java
@@ -0,0 +1,20 @@
+package com.dua3.fx.editor;
+
+import com.dua3.utility.lang.LangUtil;
+import javafx.application.Application;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Editor {
+
+	public static void main(String[] args) {
+		LangUtil.setLogLevel(Level.FINE);
+		LangUtil.setLogLevel(Level.WARNING, 
+				Logger.getLogger("com.sun"),
+				Logger.getLogger("javafx")
+			);
+		Application.launch(EditorApp.class, args);
+	}
+
+}
---
+++ fx-editor/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module com.dua3.fx.editor {
+    exports com.dua3.fx.editor;
+    opens com.dua3.fx.editor;
+    
+    requires java.logging;
+    
+    requires com.dua3.utility;
+    requires com.dua3.fx.util;
+    requires com.dua3.fx.application;
+    requires com.dua3.fx.editors;
+
+    requires info.picocli;
+
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+	requires java.desktop;
+}
