c2106ade3401bf5fb39359f41ddf7b95874002e8
Axel Howind <axel@dua3.com>
2019-07-03 09:34:38 +0200

    add windows batch

---
+++ fx-editors/node/create_bundle.bat
@@ -0,0 +1,15 @@
+@echo off
+set DIR=%~dp0
+set DIST_DIR="%DIR%/dist"
+set TARGET_DIR="%DIR%/../src/main/resources/com/dua3/fx/editors/intern"
+
+cd %DIR%
+
+echo "running webpack ..."
+npx webpack --mode=production || exit 1
+
+echo "copying files to %TARGET_DIR% ..."
+cp "%DIST_DIR%/code_editor.bundle.js" "%TARGET_DIR%/"
+cp "%DIST_DIR%/code_editor.html" "%TARGET_DIR}/"
+
+echo "SUCCESS"
