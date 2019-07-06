b31d49e528659c1e0733da3b20af7a240349a0ee
Axel Howind <axel@dua3.com>
2019-07-06 09:56:47 +0200

    formatting

--- fx-editors/node/src/code_editor.js
+++ fx-editors/node/src/code_editor.js
@@ -44,47 +44,48 @@ function trace(m) {
     }
 }
 
-// CodeMirror settings
-
+// Code taken from codemirror/loadmode extension and changed to be compatible with webpack:
 // ----<8--------<8--------<8--------<8--------<8--------<8--------<8----
 
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-    var loading = {};
-    function splitCallback(cont, n) {
-        var countDown = n;
-        return function() { if (--countDown == 0) cont(); };
+var loading = {};
+
+function splitCallback(cont, n) {
+    var countDown = n;
+    return function() { if (--countDown == 0) cont(); };
+}
+
+function ensureDeps(mode, cont) {
+    var deps = CodeMirror.modes[mode].dependencies;
+    if (!deps) return cont();
+    var missing = [];
+    for (var i = 0; i < deps.length; ++i) {
+        if (!CodeMirror.modes.hasOwnProperty(deps[i]))
+            missing.push(deps[i]);
     }
-    function ensureDeps(mode, cont) {
-        var deps = CodeMirror.modes[mode].dependencies;
-        if (!deps) return cont();
-        var missing = [];
-        for (var i = 0; i < deps.length; ++i) {
-            if (!CodeMirror.modes.hasOwnProperty(deps[i]))
-                missing.push(deps[i]);
-        }
-        if (!missing.length) return cont();
-        var split = splitCallback(cont, missing.length);
-        for (var i = 0; i < missing.length; ++i)
-            CodeMirror.requireMode(missing[i], split);
-    }
+    if (!missing.length) return cont();
+    var split = splitCallback(cont, missing.length);
+    for (var i = 0; i < missing.length; ++i)
+        CodeMirror.requireMode(missing[i], split);
+}
 
-    CodeMirror.requireMode = function(mode, cont) {
-        if (typeof mode != "string") mode = mode.name;
-        if (CodeMirror.modes.hasOwnProperty(mode)) return ensureDeps(mode, cont);
-        if (loading.hasOwnProperty(mode)) return loading[mode].push(cont);
+CodeMirror.requireMode = function(mode, cont) {
+    if (typeof mode != "string") mode = mode.name;
+    if (CodeMirror.modes.hasOwnProperty(mode)) return ensureDeps(mode, cont);
+    if (loading.hasOwnProperty(mode)) return loading[mode].push(cont);
 
-        require("codemirror/mode/"+mode+"/"+mode+".js");
-        cont();
-    };
+    require("codemirror/mode/"+mode+"/"+mode+".js");
+    cont();
+};
 
-    CodeMirror.autoLoadMode = function(instance, mode) {
-        if (!CodeMirror.modes.hasOwnProperty(mode))
-            CodeMirror.requireMode(mode, function() {
-                instance.setOption("mode", instance.getOption("mode"));
-            });
-    };
+CodeMirror.autoLoadMode = function(instance, mode) {
+    if (!CodeMirror.modes.hasOwnProperty(mode))
+        CodeMirror.requireMode(mode, function() {
+            instance.setOption("mode", instance.getOption("mode"));
+        });
+};
 
 // ---->8-------->8-------->8-------->8-------->8-------->8-------->8----
 
