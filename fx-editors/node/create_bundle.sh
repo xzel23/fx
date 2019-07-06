a38f889b65da0314a131641aa099541b60358440
Axel Howind <axel@dua3.com>
2019-07-06 08:57:59 +0200

    fix script

--- fx-editors/node/create_bundle.sh
+++ fx-editors/node/create_bundle.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
+
 DIR=`dirname $0`
 DIST_DIR=${DIR}/dist
 TARGET_DIR=${DIR}/../src/main/resources/com/dua3/fx/editors/intern
 
-cd ${DIR}
-
 echo "running webpack ..."
-npx webpack --mode=production || exit 1
+( cd ${DIR} && npx webpack --mode=production ) || exit 1
 
 echo "copying files to ${TARGET_DIR} ..."
 cp ${DIST_DIR}/code_editor.bundle.js ${TARGET_DIR}/ || exit 1
