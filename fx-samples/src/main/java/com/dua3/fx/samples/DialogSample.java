e93cd8b844679e210f8208f727bf7b75618cff1c
Axel Howind <axel@dua3.com>
2019-07-24 13:37:03 +0200

    reformat

--- fx-samples/src/main/java/com/dua3/fx/samples/DialogSample.java
+++ fx-samples/src/main/java/com/dua3/fx/samples/DialogSample.java
@@ -19,120 +19,120 @@ import java.util.Optional;
  */
 public class DialogSample extends Application {
 
-  private Button createButton(String text, Runnable action) {
-    Button btn = new Button(text);
-    btn.setOnAction(e -> action.run());
-    btn.setPrefWidth(120);
-    return btn;
-  }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-  @Override
-  public void start(Stage primaryStage) {
-    VBox container = new VBox();
+    private Button createButton(String text, Runnable action) {
+        Button btn = new Button(text);
+        btn.setOnAction(e -> action.run());
+        btn.setPrefWidth(120);
+        return btn;
+    }
 
-      // Confirmation
-      container.getChildren().add(createButton("Confirmation", () -> {
-          Dialogs.confirmation()
-                  .title("Elevator cleaning")
-                  .header("Good for you!")
-                  .text("You've decided to clean the elevator.")
-                  .showAndWait();
-      }));
+    @Override
+    public void start(Stage primaryStage) {
+        VBox container = new VBox();
 
-    // Information
-    container.getChildren().add(createButton("Info", () -> {
-      Dialogs.information()
-          .title("Info")
-              .header("Elevator cleaning")
-              .text("To clean and service the electromagnetic coils in the bottom, " +
-                      "it is necessary to jettison the access plate in the floor.")
-          .showAndWait();
-    }));
+        // Confirmation
+        container.getChildren().add(createButton("Confirmation", () -> {
+            Dialogs.confirmation()
+                    .title("Elevator cleaning")
+                    .header("Good for you!")
+                    .text("You've decided to clean the elevator.")
+                    .showAndWait();
+        }));
 
-      // Warning
-      container.getChildren().add(createButton("Warning", () -> {
-          Dialogs.warning()
-                  .title("Warning")
-                  .header("Attention... danger")
-                  .text("Automatic charges will now blow the explosive bolts in the floor plate unit. " +
-                          "The plate will disengage from the floor in 5 seconds.")
-                  .showAndWait();
-    }));
+        // Information
+        container.getChildren().add(createButton("Info", () -> {
+            Dialogs.information()
+                    .title("Info")
+                    .header("Elevator cleaning")
+                    .text("To clean and service the electromagnetic coils in the bottom, " +
+                            "it is necessary to jettison the access plate in the floor.")
+                    .showAndWait();
+        }));
 
-      // Error
-      container.getChildren().add(createButton("Error", () -> {
-          Dialogs.error()
-                  .title("Error")
-                  .header("Please leave the elevator immediately")
-                  .text("5-4-3-2-1...")
-                  .showAndWait();
-      }));
+        // Warning
+        container.getChildren().add(createButton("Warning", () -> {
+            Dialogs.warning()
+                    .title("Warning")
+                    .header("Attention... danger")
+                    .text("Automatic charges will now blow the explosive bolts in the floor plate unit. " +
+                            "The plate will disengage from the floor in 5 seconds.")
+                    .showAndWait();
+        }));
 
-    // Prompt
-    container.getChildren().add(createButton("Prompt", () -> {
-      Dialogs.prompt()
-          .title("Prompt")
-          .header("This is a prompt dialog.")
-          .showAndWait();
-    }));
+        // Error
+        container.getChildren().add(createButton("Error", () -> {
+            Dialogs.error()
+                    .title("Error")
+                    .header("Please leave the elevator immediately")
+                    .text("5-4-3-2-1...")
+                    .showAndWait();
+        }));
 
-    // Input
-    container.getChildren().add(createButton("Input", () -> {
-      Dialogs.input()
-          .title("Input")
-          .header("This is an input dialog.")
-          .string("txt", "enter text", () -> "dflt")
-          .integer("integer", "enter number", () -> 0)
-          .integer("integer from 4 to 7", "enter number", () -> 0,
-              i -> i >= 4 && i <= 7 ? Optional.empty() : Optional.of(i + " is not between 4 and 7"))
-          .comboBox("list", "choose one", () -> "Maybe", String.class, List.of("Yes", "No", "Maybe"))
-          .checkBox("bool", "Yes or No:", () -> false, "yes")
-          .chooseFile("file", "File", () -> null, InputBuilder.FileDialogMode.OPEN, new FileChooser.ExtensionFilter("all files", "*"))
-          .showAndWait();
-    }));
+        // Prompt
+        container.getChildren().add(createButton("Prompt", () -> {
+            Dialogs.prompt()
+                    .title("Prompt")
+                    .header("This is a prompt dialog.")
+                    .showAndWait();
+        }));
 
-    // Options
-    container.getChildren().add(createButton("Options", () -> {
-      var rc = Dialogs.options()
-          .options(CsvIo.getCommonOptions())
-          .title("Options")
-          .header("This is an options dialog.")
-          .showAndWait();
-      rc.ifPresent(ops -> System.out.println("RESULT:\n" + ops));
-    }));
+        // Input
+        container.getChildren().add(createButton("Input", () -> {
+            Dialogs.input()
+                    .title("Input")
+                    .header("This is an input dialog.")
+                    .string("txt", "enter text", () -> "dflt")
+                    .integer("integer", "enter number", () -> 0)
+                    .integer("integer from 4 to 7", "enter number", () -> 0,
+                            i -> i >= 4 && i <= 7 ? Optional.empty() : Optional.of(i + " is not between 4 and 7"))
+                    .comboBox("list", "choose one", () -> "Maybe", String.class, List.of("Yes", "No", "Maybe"))
+                    .checkBox("bool", "Yes or No:", () -> false, "yes")
+                    .chooseFile("file", "File", () -> null, InputBuilder.FileDialogMode.OPEN, new FileChooser.ExtensionFilter("all files", "*"))
+                    .showAndWait();
+        }));
 
-    // Wizard
-    container.getChildren().add(createButton("Wizard", () -> {
-      var rc = Dialogs.wizard()
-          .title("Database Connection Wizard")
-          .page("start",
-              Dialogs.informationPane()
-                  .header("Create new database connection")
-                  .text(
-                      "This wizard helps you to define a new database conection.%n" +
-                          "%n" +
-                          "You will need the following information:%n" +
-                          "- the vendor or manufacturer name of your database system%n" +
-                          "- the server name and port"))
-          .page("dbms",
-              Dialogs.inputPane()
-                  .header("Choose your Database from the list below.")
-                  .radioList("rdmbs", "Database", () -> null, String.class, List.of("H2", "PostgreSQL", "MySQL"))
-              )
-          .showAndWait();
-      System.out.format("Dialog result:%n%s%n", rc);
-    }));
+        // Options
+        container.getChildren().add(createButton("Options", () -> {
+            var rc = Dialogs.options()
+                    .options(CsvIo.getCommonOptions())
+                    .title("Options")
+                    .header("This is an options dialog.")
+                    .showAndWait();
+            rc.ifPresent(ops -> System.out.println("RESULT:\n" + ops));
+        }));
 
-    StackPane root = new StackPane(container);
+        // Wizard
+        container.getChildren().add(createButton("Wizard", () -> {
+            var rc = Dialogs.wizard()
+                    .title("Database Connection Wizard")
+                    .page("start",
+                            Dialogs.informationPane()
+                                    .header("Create new database connection")
+                                    .text(
+                                            "This wizard helps you to define a new database conection.%n" +
+                                                    "%n" +
+                                                    "You will need the following information:%n" +
+                                                    "- the vendor or manufacturer name of your database system%n" +
+                                                    "- the server name and port"))
+                    .page("dbms",
+                            Dialogs.inputPane()
+                                    .header("Choose your Database from the list below.")
+                                    .radioList("rdmbs", "Database", () -> null, String.class, List.of("H2", "PostgreSQL", "MySQL"))
+                    )
+                    .showAndWait();
+            System.out.format("Dialog result:%n%s%n", rc);
+        }));
 
-    Scene scene = new Scene(root);
+        StackPane root = new StackPane(container);
 
-    primaryStage.setTitle("Dialogs");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
+        Scene scene = new Scene(root);
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+        primaryStage.setTitle("Dialogs");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
