9ecb6a16220b062c618b28c59bc0b5d34748b6b8
Axel Howind <axel@dua3.com>
2019-07-25 00:20:50 +0200

    remove obsolete project

--- fx-markdowneditor/README.md
+++
@@ -1,3 +0,0 @@
-# Markdowneditor
-
-A simple Markdowneditor as a sample application of `fx-editors-markdown`.
--- fx-markdowneditor/src/main/java/com/dua3/fx/markdowneditor/cli/package-info.java
+++
@@ -1,5 +0,0 @@
-package com.dua3.fx.editor.cli;
-
-/**
- * Classes that implement handling of command line parameters.
- */
