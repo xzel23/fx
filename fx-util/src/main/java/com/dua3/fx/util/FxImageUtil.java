85771bf0d2acc424517177589c9232f34d0f69dd
Axel Howind <axel@dua3.com>
2021-06-18 19:09:34 +0200

    bump version; do not return Optional from image loading

--- fx-util/src/main/java/com/dua3/fx/util/FxImageUtil.java
+++ fx-util/src/main/java/com/dua3/fx/util/FxImageUtil.java
@@ -2,16 +2,21 @@ package com.dua3.fx.util;
 
 import com.dua3.utility.data.ImageUtil;
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelFormat;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.Optional;
 
 public final class FxImageUtil implements ImageUtil<Image> {
     
     @Override
-    public Optional<FxImage> load(InputStream inputStream) throws IOException {
-        return Optional.of(new FxImage(new Image(inputStream)));
+    public FxImage load(InputStream inputStream) throws IOException {
+        return new FxImage(new Image(inputStream));
     }
 
     @Override
@@ -26,5 +31,12 @@ public final class FxImageUtil implements ImageUtil<Image> {
     public FxImage convert(Image img) {
         return new FxImage(img);
     }
-    
+
+    @Override
+    public FxImage create(int w, int h, int[] argb) {
+        WritableImage wr = new WritableImage(w, h);
+        PixelWriter pw = wr.getPixelWriter();
+        pw.setPixels(0, 0, w, h, PixelFormat.getIntArgbInstance(), argb, 0, w);
+        return new FxImage(wr);
+    }    
 }
