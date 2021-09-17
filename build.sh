fbf1ba1e4a240b27d9b5eb0040ac308b42336e05
Axel Howind <axel@dua3.com>
2021-09-17 16:07:21 +0200

    remove publish from default tasks

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd `dirname $0`
 export JAVA_HOME=`/usr/libexec/java_home -v 17`
-bash ./gradlew clean build publishToMavenLocal publish
+bash ./gradlew clean build publishToMavenLocal $*
