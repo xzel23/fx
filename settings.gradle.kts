5db9a7cd5a73b99a9131078245bf8362c7b5104c
Axel Howind <axel@dua3.com>
2022-07-08 10:49:45 +0200

    migrate to kotlin DSL

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "fx"
+
+include("fx-util")
+include("fx-util-db")
+include("fx-application")
+include("fx-controls")
+include("fx-icons")
+include("fx-icons:fx-icons-ikonli")
+include("fx-web")
+// samples and apps
+include("fx-samples")
