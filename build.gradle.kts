5118161b74611df7224414f4d92786bc853e7999
Axel Howind <axel@dua3.com>
2022-07-22 13:17:23 +0200

    release as 0.17.0-RC6.1 without code changes because of packaging problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ plugins {
 /////////////////////////////////////////////////////////////////////////////
 object meta {
     val group           = "com.dua3.fx"
-    val version         = "0.17.0-RC6-SNAPSHOT"
+    val version         = "0.17.0-RC6.1"
     val scm             = "https://gitlab.com/com.dua3/lib/fx.git"
     val repo            = "public"
     val licenseName     = "The Apache Software License, Version 2.0"
