82b6f794ff579f7e414f11d6a152f449f5e82c7d
Axel Howind <axh@dua3.com>
2023-02-02 20:03:54 +0100

    bump version; update plugins, dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "dua3-fx"
-val projectVersion = "0.21.1"
+val projectVersion = "0.21.2-SNAPSHOT"
 
 include("fx-util")
 include("fx-util-db")
@@ -19,14 +19,14 @@ dependencyResolutionManagement {
         create("libs") {
             version("projectVersion", projectVersion)
 
-            plugin("versions", "com.github.ben-manes.versions").version("0.44.0")
+            plugin("versions", "com.github.ben-manes.versions").version("0.45.0")
             plugin("test-logger", "com.adarshr.test-logger").version("3.2.0")
             plugin("spotbugs", "com.github.spotbugs").version("5.0.13")
             plugin("cabe", "com.dua3.cabe").version("1.0.0")
             plugin("javafx", "org.openjfx.javafxplugin").version("0.0.13")
 
             version("cabe", "1.0.0")
-            version("dua3-utility", "10.2.5")
+            version("dua3-utility", "10.2.6")
             version("javafx", "19")
             version("ikonli", "12.3.1")
             version("junit", "5.9.2")
