946d30e79b50d805866a3d1f6c9bab351008f182
Axel Howind <axh@dua3.com>
2023-02-22 13:52:53 +0100

    update plugins; dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,18 +19,18 @@ dependencyResolutionManagement {
         create("libs") {
             version("projectVersion", projectVersion)
 
-            plugin("versions", "com.github.ben-manes.versions").version("0.45.0")
+            plugin("versions", "com.github.ben-manes.versions").version("0.46.0")
             plugin("test-logger", "com.adarshr.test-logger").version("3.2.0")
             plugin("spotbugs", "com.github.spotbugs").version("5.0.13")
             plugin("cabe", "com.dua3.cabe").version("1.2.0")
             plugin("javafx", "org.openjfx.javafxplugin").version("0.0.13")
 
             version("cabe", "1.0.0")
-            version("dua3-utility", "10.2.9")
+            version("dua3-utility", "10.3.1")
             version("javafx", "19")
             version("ikonli", "12.3.1")
             version("junit", "5.9.2")
-            version("log4j", "2.19.0")
+            version("log4j", "2.20.0")
             version("slf4j", "2.0.6")
 
             library("cabe-annotations", "com.dua3.cabe", "cabe-annotations").versionRef("cabe")
