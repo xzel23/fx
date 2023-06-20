8e98804be37151f2c82ba6a6ce462df2da8995f4
Axel Howind <axh@dua3.com>
2023-06-20 08:49:56 +0200

    fix PinBoard content clipping

--- fx-controls/src/main/java/com/dua3/fx/controls/PinBoard.java
+++ fx-controls/src/main/java/com/dua3/fx/controls/PinBoard.java
@@ -230,10 +230,12 @@ class PinBoardSkin extends SkinBase<PinBoard> {
         double dx = Math.max(0, viewPort.getWidth() - boardArea.getWidth()) / 2.0;
         double dy = Math.max(0, viewPort.getHeight() - boardArea.getHeight()) / 2.0;
 
+        Rectangle2D viewportInLocal = new Rectangle2D(viewPort.getMinX()+boardArea.getMinX(), viewPort.getMinY()+boardArea.getMinY(), viewPort.getWidth(), viewPort.getHeight());
+
         // populate pane with nodes of visible items
         List<Node> nodes = new ArrayList<>(board.items) // copy list to avoid concurrent modification
                 .stream()
-                .filter(item -> item.area().intersects(viewPort))
+                .filter(item -> item.area().intersects(viewportInLocal))
                 .map(item -> {
                     LOG.debug("item is visible: {}", item.name());
                     Rectangle2D itemArea = item.area();
