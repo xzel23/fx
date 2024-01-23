cbf3890bc8461dbe06740cc6bcd7042ac927f37b
Axel Howind <axh@dua3.com>
2024-01-23 10:05:25 +0100

    v0.33

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "dua3-fx"
-val projectVersion = "0.33-SNAPSHOT"
+val projectVersion = "0.33"
 
 include("fx-util")
 include("fx-util-db")
@@ -27,7 +27,7 @@ dependencyResolutionManagement {
             plugin("javafx", "org.openjfx.javafxplugin").version("0.1.0")
 
             version("cabe", "2.0")
-            version("dua3-utility", "12.0.3")
+            version("dua3-utility", "12.0.4")
             version("javafx", "21.0.2")
             version("ikonli", "12.3.1")
             version("junit", "5.10.1")
