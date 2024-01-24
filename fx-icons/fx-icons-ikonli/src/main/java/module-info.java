4196c7668d5cd42051932fa4b0aa382deb8e91f5
Axel Howind <axh@dua3.com>
2024-01-24 10:23:20 +0100

    add javadoc

--- fx-icons/fx-icons-ikonli/src/main/java/module-info.java
+++ fx-icons/fx-icons-ikonli/src/main/java/module-info.java
@@ -1,3 +1,17 @@
+/**
+ * This module is a Java module that provides integration of the Ikonli library with JavaFX Icons.
+ * It exports the package com.dua3.fx.icons.ikonli and opens the same package for reflective access.
+ *
+ * This module requires the following external dependencies:
+ * - org.apache.logging.log4j
+ * - org.kordamp.ikonli.core
+ * - org.kordamp.ikonli.javafx
+ * - com.dua3.fx.icons
+ * - javafx.graphics
+ *
+ * This module also uses the interface org.kordamp.ikonli.IkonHandler and provides an implementation
+ * of the com.dua3.fx.icons.IconProvider interface with the class com.dua3.fx.icons.ikonli.IkonliIconProvider.
+ */
 module com.dua3.fx.icons.ikonli {
     exports com.dua3.fx.icons.ikonli;
     opens com.dua3.fx.icons.ikonli;
