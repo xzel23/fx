98f85e6f1b4d53f984669e56bd7c274580bb02ec
Axel Howind <axh@dua3.com>
2024-02-02 20:05:35 +0100

    update gradle, cabe, utility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,11 +23,11 @@ dependencyResolutionManagement {
             plugin("versions", "com.github.ben-manes.versions").version("0.51.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
             plugin("spotbugs", "com.github.spotbugs").version("6.0.7")
-            plugin("cabe", "com.dua3.cabe").version("2.1")
+            plugin("cabe", "com.dua3.cabe").version("2.1.2")
             plugin("javafx", "org.openjfx.javafxplugin").version("0.1.0")
 
             version("cabe", "2.0")
-            version("dua3-utility", "12.0.6")
+            version("dua3-utility", "12.0.7")
             version("javafx", "21.0.2")
             version("ikonli", "12.3.1")
             version("junit", "5.10.1")
