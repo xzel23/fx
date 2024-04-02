37039d6276555af2d2d414abd2a8bbb084f5652c
Axel Howind <axh@dua3.com>
2024-04-02 05:36:53 +0200

    update utility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ dependencyResolutionManagement {
             plugin("javafx", "org.openjfx.javafxplugin").version("0.1.0")
 
             version("cabe", "2.0")
-            version("dua3-utility", "12.1.0")
+            version("dua3-utility", "12.1.1")
             version("javafx", "22")
             version("ikonli", "12.3.1")
             version("log4j", "2.23.1")
