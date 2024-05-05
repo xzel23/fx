cc44e0a520eeebcf03d21f949b5c934627729b26
Axel Howind <axh@dua3.com>
2024-05-05 22:54:53 +0200

    remove exclusions after spotbugs fixed false positives

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,13 +22,13 @@ dependencyResolutionManagement {
 
             plugin("versions", "com.github.ben-manes.versions").version("0.51.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
-            plugin("spotbugs", "com.github.spotbugs").version("6.0.12")
+            plugin("spotbugs", "com.github.spotbugs").version("6.0.14")
             plugin("cabe", "com.dua3.cabe").version("2.1.2")
             plugin("javafx", "org.openjfx.javafxplugin").version("0.1.0")
 
             version("cabe", "2.0")
             version("commons-logging", "1.3.1")
-            version("dua3-utility", "13.0-SNAPSHOT")
+            version("dua3-utility", "13.0-BETA")
             version("javafx", "22")
             version("ikonli", "12.3.1")
             version("log4j", "2.23.1")
