e6008fde7eaa4dcb83c4ca60d38c125eaeb68d76
Axel Howind <axh@dua3.com>
2024-07-04 09:23:07 +0200

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "dua3-fx"
-val projectVersion = "0.41.0-BETA1"
+val projectVersion = "0.41.0-BETA2"
 
 include("fx-application")
 include("fx-application:fx-application-fxml")
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
             version("cabe", "2.0")
             version("commons-logging", "1.3.1")
             version("dua3-utility", "13.0-BETA5")
-            version("javafx", "22")
+            version("javafx", "22.0.1")
             version("log4j", "2.23.1")
             version("slf4j", "2.0.13")
 
