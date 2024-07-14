9d25faecb783609454c931e1b960ed148f5db078
Axel Howind <axh@dua3.com>
2024-07-14 18:27:57 +0200

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "dua3-fx"
-val projectVersion = "0.41.0-BETA3"
+val projectVersion = "0.41.0-BETA4-SNAPSHOT"
 
 include("fx-application")
 include("fx-application:fx-application-fxml")
