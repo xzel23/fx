3caaa19fed6b82f3225681f78dd553d430ac4888
Axel Howind <axh@dua3.com>
2024-09-20 13:44:33 +0200

    v0.44; JavaFX 23; utility 13.1.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "dua3-fx"
-val projectVersion = "0.44.0-SNAPSHOT"
+val projectVersion = "0.44"
 
 include("fx-application")
 include("fx-application:fx-application-fxml")
@@ -19,8 +19,8 @@ dependencyResolutionManagement {
 
             version("cabe", "2.0")
             version("commons-logging", "1.3.1")
-            version("dua3-utility", "13.0.2")
-            version("javafx", "22.0.1")
+            version("dua3-utility", "13.1.1")
+            version("javafx", "23")
             version("log4j", "2.23.1")
             version("slf4j", "2.0.13")
 
