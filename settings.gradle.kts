8b51500974446d247ae66ee3a873c1a893b16fa9
Axel Howind <axh@dua3.com>
2024-11-21 10:34:12 +0100

    update gradle, dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "dua3-fx"
-val projectVersion = "0.45-beta-2"
+val projectVersion = "0.45-rc"
 
 include("fx-application")
 include("fx-application:fx-application-fxml")
@@ -21,10 +21,10 @@ dependencyResolutionManagement {
             plugin("versions", "com.github.ben-manes.versions").version("0.51.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
             plugin("spotbugs", "com.github.spotbugs").version("6.0.26")
-            plugin("cabe", "com.dua3.cabe").version("3.0-beta-10")
+            plugin("cabe", "com.dua3.cabe").version("3.0-rc")
 
             version("commons-logging", "1.3.1")
-            version("dua3-utility", "14-beta-3")
+            version("dua3-utility", "14")
             version("javafx", "23")
             version("jspecify", "1.0.0")
             version("log4j", "2.24.1")
