968e1fcc20a1ba05172d686d528632e172e8a697
Axel Howind <axh@dua3.com>
2025-01-29 11:25:29 +0100

    v1.0.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "dua3-fx"
-val projectVersion = "1.0.4-RC1"
+val projectVersion = "1.0.4"
 
 include("fx-application")
 include("fx-application:fx-application-fxml")
@@ -20,7 +20,7 @@ dependencyResolutionManagement {
             plugin("spotbugs", "com.github.spotbugs").version("6.1.3")
             plugin("cabe", "com.dua3.cabe").version("3.0.2")
 
-            version("dua3-utility", "15.2.0-RC3")
+            version("dua3-utility", "16")
             version("javafx", "23")
             version("jspecify", "1.0.0")
             version("log4j-bom", "2.24.3")
