b39c7b93b1752d3e6153ff1ed300634395f1d4b9
Axel Howind <axh@dua3.com>
2025-03-06 11:07:58 +0100

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,10 +17,10 @@ dependencyResolutionManagement {
 
             plugin("versions", "com.github.ben-manes.versions").version("0.52.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
-            plugin("spotbugs", "com.github.spotbugs").version("6.1.5")
+            plugin("spotbugs", "com.github.spotbugs").version("6.1.7")
             plugin("cabe", "com.dua3.cabe").version("3.0.2")
 
-            version("dua3-utility", "16.1.2")
+            version("dua3-utility", "16.1.3")
             version("javafx", "23")
             version("jspecify", "1.0.0")
             version("log4j-bom", "2.24.3")
