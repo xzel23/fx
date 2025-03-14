dce6953a0010bf4da435f6ac33b093e991594da8
Axel Howind <axh@dua3.com>
2025-03-14 09:26:26 +0100

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "dua3-fx"
-val projectVersion = "1.0.7"
+val projectVersion = "1.0.8-SNAPSHOT"
 
 include("fx-application")
 include("fx-application:fx-application-fxml")
