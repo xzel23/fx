3a259311f7122c6fdae66583a8963cf897dd98e7
Axel Howind <axh@dua3.com>
2025-03-26 15:33:24 +0100

    update utility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ dependencyResolutionManagement {
             plugin("spotbugs", "com.github.spotbugs").version("6.1.7")
             plugin("cabe", "com.dua3.cabe").version("3.0.2")
 
-            version("dua3-utility", "16.2.0")
+            version("dua3-utility", "17.0.0")
             version("javafx", "23")
             version("jspecify", "1.0.0")
             version("log4j-bom", "2.24.3")
