435f18b9c79e8732634a68ccfa2fe0e43c74f799
Axel Howind <axh@dua3.com>
2025-04-30 07:16:51 +0200

    update spotbugs plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
 
             plugin("versions", "com.github.ben-manes.versions").version("0.52.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
-            plugin("spotbugs", "com.github.spotbugs").version("6.1.7")
+            plugin("spotbugs", "com.github.spotbugs").version("6.1.10")
             plugin("cabe", "com.dua3.cabe").version("3.0.2")
             plugin("forbiddenapis", "de.thetaphi.forbiddenapis").version("3.9")
 
