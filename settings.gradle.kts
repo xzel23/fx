a88bb00c7bf78b28f95da678cb3de9a70d96b1f5
Axel Howind <axh@dua3.com>
2025-05-12 06:18:01 +0200

    bump version; update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "dua3-fx"
-val projectVersion = "1.3.3-SNAPSHOT"
+val projectVersion = "1.4.0-SNAPSHOT"
 
 include("fx-application")
 include("fx-application:fx-application-fxml")
@@ -17,12 +17,12 @@ dependencyResolutionManagement {
 
             plugin("versions", "com.github.ben-manes.versions").version("0.52.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
-            plugin("spotbugs", "com.github.spotbugs").version("6.1.10")
+            plugin("spotbugs", "com.github.spotbugs").version("6.1.11")
             plugin("cabe", "com.dua3.cabe").version("3.0.2")
             plugin("forbiddenapis", "de.thetaphi.forbiddenapis").version("3.9")
 
-            version("dua3-utility", "18.3.1")
-            version("javafx", "23")
+            version("dua3-utility", "19.0.0")
+            version("javafx", "24")
             version("jspecify", "1.0.0")
             version("log4j-bom", "2.24.3")
             version("spotbugs", "4.9.3")
