1a5686569b14c01dfa15b8da2c7148a3439a3857
Axel Howind <axh@dua3.com>
2025-07-14 06:34:00 +0200

    start development of beta3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "dua3-fx"
-val projectVersion = "1.5.0-beta2"
+val projectVersion = "1.5.0-beta3-snapshot"
 
 include("fx-application")
 include("fx-application:fx-application-fxml")
