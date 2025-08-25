71de6ce54d59426b01f039bd1f95e2a90d9ffe49
Axel Howind <axh@dua3.com>
2025-08-25 08:04:31 +0200

    v1.5.0-beta6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "dua3-fx"
-val projectVersion = "1.5.0-beta6-SNAPSHOT"
+val projectVersion = "1.5.0-beta6"
 
 include("fx-application")
 include("fx-application:fx-application-fxml")
