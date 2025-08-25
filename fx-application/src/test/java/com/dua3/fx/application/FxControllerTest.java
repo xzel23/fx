f8cca5493ec4639eb27930245ab18351fad98fa3
Axel Howind <axh@dua3.com>
2025-08-25 08:27:15 +0200

    remove import

--- fx-application/src/test/java/com/dua3/fx/application/FxControllerTest.java
+++ fx-application/src/test/java/com/dua3/fx/application/FxControllerTest.java
@@ -5,7 +5,6 @@ import javafx.stage.FileChooser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.net.URI;
 import java.util.Collections;
 import java.util.List;
