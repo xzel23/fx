a25b3600832829ceafb39781c06b9b287a97bb73
Axel Howind <axh@dua3.com>
2025-10-06 15:36:08 +0200

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -85,6 +85,8 @@ dependencyResolutionManagement {
         if (isSnapshot) {
             println("snapshot version detected, adding Maven snapshot repositories")
 
+            mavenLocal()
+
             // Sonatype Snapshots
             maven {
                 name = "Central Portal Snapshots"
