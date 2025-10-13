92b5ff06d7908d34f08261d928308a9b376059e6
Axel Howind <axh@dua3.com>
2025-10-13 07:29:18 +0200

    bump major version because of API changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "dua3-fx"
-val projectVersion = "1.5.0-beta14"
+val projectVersion = "2.0.0-SNAPSHOT"
 
 include("fx-application")
 include("fx-application:fx-application-fxml")
